37003e277a63905a45c37c5eb09473cdffd310c7
Younes SERRAJ <younes.serraj@gmail.com>
2018-02-08 18:09:32 +0100

    Bugfix: removed unwanted argument

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def self.expose(name, value = nil, decorate: false, guess_value: true, scope: :all)
-    Exposed.new(self, name, value, guess_value: guess_value, scope: scope).tap do |e|
+    Exposed.new(name, value, guess_value: guess_value, scope: scope).tap do |e|
       define_method e.name do
         e.call(self).value
       end
